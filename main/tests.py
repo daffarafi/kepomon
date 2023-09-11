83e4f4c37456259257622809b6d5fac5b6a82bf3
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-09-11 14:11:15 +0700

    add test

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_app_name_in_main(self) :
+        response = Client().get('/main/')
+        self.assertContains(response, "Kepomon")
+
+    def test_name_in_main(self) :
+        response = Client().get('/main/')
+        self.assertContains(response, "Muhammad Daffa&#x27;I Rafi Prasetyo")
+
+    def test_class_in_main(self) :
+        response = Client().get('/main/')
+        self.assertContains(response, "PBP A")

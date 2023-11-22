e7fd713c8557a209e0bc030ffc3006062106cb0c
Muhammad Daffa'I Rafi Prasetyo <daffarafip24@gmail.com>
2023-11-22 11:14:41 +0700

    fix auth

--- kepomon/urls.py
+++ kepomon/urls.py
@@ -25,4 +25,5 @@ urlpatterns = [
     path('json/', show_json, name='show_json'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
+    path('auth/', include('authentication.urls'))
 ]

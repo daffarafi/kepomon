c0f2c934c77d95aeceac554d16a032e1df24be02
daffarafip24 <daffarafip24@gmail.com>
2024-08-28 06:08:21 +0700

    test commit

--- authentication/views.py
+++ authentication/views.py
@@ -29,7 +29,7 @@ def login(request):
     else:
         return JsonResponse({
             "status": False,
-            "message": "Login gagal, periksa kembali email atau kata sandi."
+            "message": "Login gagal, periksa kembali email atau kata sandi!"
         }, status=401)
 
 
